6c5b297d041c10cf5e791bcf50486118bc1ee74c
KJ Price <kjprice12@gmail.com>
2023-02-03 18:08:05 -0600

    Import nltk

--- python/wordle_hack.py
+++ python/wordle_hack.py
@@ -1,5 +1,12 @@
+from nltk.corpus import words
 
-def hack_wordle(words: str):
+all_words = words.words()
+print(len(all_words))
+
+FIVE_CHAR_WORDS = [word for word in all_words if len(word) == 5]
+print(len(FIVE_CHAR_WORDS))
+
+def hack_wordle(word: str):
   pass
 
 if __name__ == '__main__':
