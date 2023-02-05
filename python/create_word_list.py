a329c43ef6cfc62dbe18a8f52b39269a8c14f275
KJ Price <kjprice12@gmail.com>
2023-02-04 21:17:11 -0600

    Create word_counts list file

--- python/create_word_list.py
+++ python/create_word_list.py
@@ -1,11 +1,17 @@
+from collections import Counter
+import re
 from typing import List
 
 # conda install -c conda-forge pattern
 from pattern.en import verbs
 
 from nltk.corpus import wordnet, words
-from nltk import pos_tag
-import pandas as pd
+from nltk.corpus import europarl_raw
+from nltk.corpus import movie_reviews
+from nltk.corpus import nps_chat
+from nltk.corpus import state_union
+from nltk.corpus import twitter_samples
+from nltk.corpus import webtext
 
 def word_exists(word: str) -> bool:
     return len(wordnet.synsets(word)) > 0
@@ -77,7 +83,59 @@ def create_word_list():
 
     return unique_five_char_words
 
+def get_all_corpora_text():
+    corpora_tokens = [
+        list(nps_chat.words()),
+        list(europarl_raw.english.words()),
+        list(state_union.words()),
+        list(webtext.words()),
+        list(movie_reviews.words()),
+        *twitter_samples.tokenized(),
+    ]
+
+    all_words = []
+    for corpus in corpora_tokens:
+        for word in corpus:
+            all_words.append(word.lower())
+    return all_words
+
+def split_words(word):
+    words = re.split('[^a-z]*', word)
+    return [word for word in words if len(word) >= 5]
+
+def get_all_texts_with_more_than_five_chars():
+    all_words = get_all_corpora_text()
+    at_least_five_char_words = [word for word in all_words if len(word) >= 5]
+
+    cleaned_words = []
+    for raw_word in at_least_five_char_words:
+        for cleaned_word in split_words(raw_word):
+            cleaned_words.append(cleaned_word)
+    
+    five_letter_words = [word for word in cleaned_words if len(word) == 5]
+    return Counter(five_letter_words)
+
+def get_all_words_with_count_frequencies():
+    all_words_text = create_word_list()
+    counter = get_all_texts_with_more_than_five_chars()
+    words_with_frequencies = {}
+    for word in all_words_text:
+        count = 0
+        if word in counter:
+            count = counter[word]
+        words_with_frequencies[word] = count
+    return Counter(words_with_frequencies)
+
+
+
 if __name__ == '__main__':
-    word_list = create_word_list()
-    with open('words.txt', 'w') as f:
-        f.write('\n'.join(word_list))
+    # word_list = create_word_list()
+    # with open('words.txt', 'w') as f:
+    #     f.write('\n'.join(word_list))
+    words_counts = get_all_words_with_count_frequencies()
+    words_count_filepath = 'words_counts.txt'
+    with open(words_count_filepath, 'w') as f:
+        f.write('')
+    with open(words_count_filepath, 'a') as f:
+        for word, count in words_counts.most_common():
+            f.write(f'{word} {count}\n')
